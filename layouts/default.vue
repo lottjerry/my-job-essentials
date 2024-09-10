0c094db63e9ba0cc95e818670046d59c99b332fd
Jerry Lott <jj@Jerrys-Mac-mini.local>
2024-09-10 17:39:08 -0500

    Finished UI for Login Page

---
+++ layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="d-flex justify-center align-center h-screen">
+    <slot />
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="d-flex flex-column align-center justify-center border ma-10">
-    <div class="d-flex flex-column align-center justify-center">
-      <h1 class="text-h3 text-center">Work Essentials</h1>
-      <div class="d-flex flex-column border">
+  <div class="d-flex flex-column align-center mx-13 justify-center">
+    <div class="d-flex flex-column align-center justify-center mb-5">
+      <h1 class="text-h3 text-center mb-5">Work Essentials</h1>
+
+      <div class="d-flex flex-column">
         <div class="d-flex justify-center">
           <Icon
             name="material-symbols:calendar-month-outline-sharp"
             size="28"
           />
         </div>
+
         <div class="d-flex ga-10 justify-center">
           <Icon name="material-symbols:qr-code-scanner" size="28" />
+
           <Icon name="material-symbols:barcode-scanner" size="28" />
         </div>
+
         <div class="d-flex justify-center">
           <Icon
             name="material-symbols:barcode-reader-outline-sharp"
@@ -21,7 +25,8 @@
         </div>
       </div>
     </div>
-    <v-sheet class="pa-2 mx-auto mt-5 border" width="250">
+
+    <v-sheet class="w-100 mx-auto mt-2">
       <v-form @submit="onSubmit" @submit.prevent class="d-flex flex-column">
         <v-text-field
           class="text-black"
@@ -46,13 +51,26 @@
           @click:append-inner="visible = !visible"
         ></v-text-field>
 
-        <v-btn class="bg-primary mt-5" type="submit">LOGIN</v-btn>
+        <v-btn class="bg-primary text-body-2 mt-5" type="submit">Log In</v-btn>
       </v-form>
     </v-sheet>
-    <div class="d-flex flex-column ga-3 mt-5">
+
+    <div class="my-3">
+      <h5>OR</h5>
+    </div>
+
+    <v-btn class="text-body-2 w-100">
+      <Icon name="logos:google-icon" class="mr-3" />
+
+      Continue with Google
+    </v-btn>
+
+    <div class="d-flex flex-column ga-5 mt-10">
       <p class="font-weight-bold text-body-2">Forgot Password?</p>
+
       <div class="text-body-2 d-flex ga-1">
         <p>Don't have an account?</p>
+
         <p class="font-weight-bold">Create account</p>
       </div>
     </div>
@@ -63,14 +81,19 @@
   import Swal from 'sweetalert2'
 
   const visible = ref(false)
+
   const email = ref('')
+
   const password = ref('')
 
   const onSubmit = (values) => {
     // display form values on success
+
     Swal.fire({
       title: 'Success !!!',
+
       text: `Email: ${email.value} Password: ${password.value}`,
+
       icon: 'success',
     })
   }

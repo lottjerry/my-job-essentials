8586ee68ad7d8c4f815beb4fcec58b025c39743d
Jerry Lott <lottjerry@gmail.com>
2024-10-07 19:32:55 -0500

    v-stepper for the schedule page is now working

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,7 +1,8 @@
 <template>
   <v-container class="d-flex h-screen">
     <v-card variant="flat" height="500" width="350">
-      <v-stepper class="mt-12" v-model="step" non-linear flat>
+      <v-stepper class="mt-12" v-model="step" non-linear flat hide-actions>
+        <!-- V Stepper Header Start -->
         <v-stepper-header>
           <v-stepper-item
             :value="1"
@@ -40,8 +41,45 @@
             edit-icon="mdi-calendar-check-outline"
           >
           </v-stepper-item>
-        </v-stepper-header> </v-stepper
-    ></v-card>
+        </v-stepper-header>
+        <!-- V Stepper Header End -->
+         <!-- ************************************************************ -->
+        <!-- V Stepper Window Start -->
+        <v-stepper-window>
+          <!-- Step 1 -->
+          <v-stepper-window-item :value="1">
+            <v-card title="Previous" flat class="d-flex flex-column ga-5">
+              <p>
+                In order to view your schedule, you will have to register with
+                your organization. This information can be obtained from your
+                office personal.
+              </p>
+            </v-card>
+          </v-stepper-window-item>
+          <!-- Step 2 -->
+          <v-stepper-window-item :value="2">
+            <v-card title="Current" flat class="d-flex flex-column ga-5">
+              <p>
+                In order to view your schedule, you will have to register with
+                your organization. This information can be obtained from your
+                office personal.
+              </p>
+            </v-card>
+          </v-stepper-window-item>
+          <!-- Step 3 -->
+          <v-stepper-window-item :value="3">
+            <v-card title="Next" flat class="d-flex flex-column ga-5">
+              <p>
+                In order to view your schedule, you will have to register with
+                your organization. This information can be obtained from your
+                office personal.
+              </p>
+            </v-card>
+          </v-stepper-window-item>
+        </v-stepper-window>
+        <!-- V Stepper Window End -->
+      </v-stepper></v-card
+    >
   </v-container>
 </template>
 <script setup>

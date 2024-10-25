e90d75e873523042a30cdffeea07e8dae94129cb
Jerry Lott <lottjerry@gmail.com>
2024-10-24 22:52:21 -0500

    Added firebase and vuefire

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import { auth } from "firebase-admin";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
@@ -7,5 +9,22 @@ export default defineNuxtConfig({
     '@nuxt/icon',
     '@vee-validate/nuxt',
     '@pinia/nuxt',
-  ]
+    'nuxt-vuefire',
+  ],
+  vuefire: {
+    auth: {
+      enabled: true,
+      sessionCookie: true
+    },
+    config: {
+      apiKey: process.env.apiKey,
+      authDomain: process.env.authDomain,
+      projectId: process.env.projectId,
+      storageBucket: process.env.storageBucket,
+      messagingSenderId: process.env.messagingSenderId,
+      appId: process.env.appId,
+      measurementId: process.env.measurementId 
+      // there could be other properties depending on the project
+    },
+  },
 })
